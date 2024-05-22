31a4b9f76a482c51864f51cffd62b99c72bd76ea
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-05-22 16:00:16 +0530

    Deleted project1

--- project1/app/views/todos/_todo.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! todo, :id, :task, :created_at, :updated_at
-json.url todo_url(todo, format: :json)
--- project1/db/migrate/20240521195008_create_todos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTodos < ActiveRecord::Migration[7.1]
-  def change
-    create_table :todos do |t|
-      t.string :task
-
-      t.timestamps
-    end
-  end
-end
